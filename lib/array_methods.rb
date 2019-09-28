e6956cb74a1d4efeca95648328ba7cddb26b57fe
Rico Mitchell <ricomitch@icloud.com>
2019-09-28 02:26:29 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,22 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  counter = 0   
+  while array[counter] do
+  return array.index(value_to_find)
+  counter += 1
+end
 end
 
 def find_max_value(array)
-  # Add your solution here
+  counter = 0
+  while array[counter] do
+  return p array.sort.last
+  break
+end 
 end
 
 def find_min_value(array)
-  # Add your solution here
+  counter = 0
+  while array[counter] do
+  return p array.sort.first
+end  
 end
